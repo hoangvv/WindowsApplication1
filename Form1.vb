e386775da875598c5e1f37db22547556b0dcafd5
hoangvv <nhathoangcabrina@gmail.com>
2015-06-06 08:38:22 +0700

    sad
    s

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+End Class
